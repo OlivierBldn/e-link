ea4d19a6efc8abcf0aba7179e4ff2c07bf28ebfd
OlivierBldn <blandin.developpement@gmail.com>
2024-09-26 19:30:42 +0200

    👌 IMPROVE: TagIndex screen - Added devices scan

--- lib/screens/TagIndex.dart
+++ lib/screens/TagIndex.dart
@@ -1,3 +1,106 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_application_1/constants/colors.dart';
+// import 'package:flutter_application_1/providers/ble_device_provider.dart';
+// import 'package:flutter_application_1/widgets/AppSearchBar.dart';
+// import 'package:flutter_application_1/widgets/NavBar.dart';
+// import 'package:flutter_application_1/widgets/TagCard.dart';
+// import 'package:provider/provider.dart';
+
+// import '../widgets/AppTitle.dart';
+
+// class TagIndex extends StatefulWidget {
+//   const TagIndex({super.key});
+
+//   @override
+//   _TagIndexState createState() => _TagIndexState();
+// }
+
+// class _TagIndexState extends State<TagIndex> {
+//   final TextEditingController _searchController = TextEditingController();
+//   String _searchQuery = '';
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     final bleProvider = Provider.of<BleDeviceProvider>(context, listen: false);
+//     bleProvider.startBleScan();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppColors.backgroundColor,
+//       body: ListView(
+//         children: [
+//           const AppTitle(
+//             blackText: 'Your ',
+//             purpleText: 'Tags',
+//           ),
+//           const AppSearchBar(
+//             placeholder: 'Please enter a tag name or MAC',
+//           ),
+//            Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: TextField(
+//               controller: _searchController,
+//               decoration: InputDecoration(
+//                 labelText: 'Search by MAC Address',
+//                 prefixIcon: const Icon(Icons.search),
+//                 border: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(8),
+//                 ),
+//               ),
+//               onChanged: (value) {
+//                 setState(() {
+//                   _searchQuery = value;
+//                 });
+//               },
+//             ),
+//           ),
+//           Container(
+//             margin: const EdgeInsets.only(
+//               left: 28,
+//               right: 28,
+//               top: 20,
+//             ),
+//             child: Consumer<BleDeviceProvider>(
+//               builder: (context, provider, child) {
+//                 return Column(
+//                   children: provider.devices.map((device) {
+//                     return TagCard(
+//                       title: device.name,
+//                       available: provider.isConnected && device.mac == provider.connectedDevice?.mac,
+//                       screenSize: 7.5,
+//                       resolutionX: 800,
+//                       resolutionY: 480,
+//                       colorMode: 'BWR',
+//                       mac: device.mac,
+
+//                       battery: '${device.battery}%',
+//                     );
+//                   }).toList(),
+//                 );
+//               },
+//             ),
+//           ),
+//           const NavBar(selectedMenu: 'tags')
+//         ],
+//       ),
+//     );
+//   }
+//   @override
+//   void dispose() {
+//     _searchController.dispose();
+//     super.dispose();
+//   }
+// }
+
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/providers/ble_device_provider.dart';
@@ -12,82 +115,120 @@ class TagIndex extends StatefulWidget {
   const TagIndex({super.key});
 
   @override
-  _TagIndexState createState() => _TagIndexState();
+  TagIndexState createState() => TagIndexState();
 }
 
-class _TagIndexState extends State<TagIndex> {
+class TagIndexState extends State<TagIndex> {
   final TextEditingController _searchController = TextEditingController();
   String _searchQuery = '';
+  bool _isScanning = false;
 
-  @override
-  void initState() {
-    super.initState();
-    final bleProvider = Provider.of<BleDeviceProvider>(context, listen: false);
-    bleProvider.startBleScan();
+  Future<void> _startScan(BleDeviceProvider bleProvider) async {
+    setState(() {
+      _isScanning = true;
+    });
+    await bleProvider.startBleScan();
+    setState(() {
+      _isScanning = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final bleProvider = Provider.of<BleDeviceProvider>(context, listen: false);
+
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: ListView(
+      body: Column(
         children: [
-          const AppTitle(
-            blackText: 'Your ',
-            purpleText: 'Tags',
-          ),
-          const AppSearchBar(
-            placeholder: 'Please enter a tag name or MAC',
-          ),
-           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                labelText: 'Search by MAC Address',
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
+          Expanded(
+            child: ListView(
+              children: [
+                const AppTitle(
+                  blackText: 'Your ',
+                  purpleText: 'Tags',
                 ),
-              ),
-              onChanged: (value) {
-                setState(() {
-                  _searchQuery = value;
-                });
-              },
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(
-              left: 28,
-              right: 28,
-              top: 20,
-            ),
-            child: Consumer<BleDeviceProvider>(
-              builder: (context, provider, child) {
-                return Column(
-                  children: provider.devices.map((device) {
-                    return TagCard(
-                      title: device.name,
-                      available: provider.isConnected && device.mac == provider.connectedDevice?.mac,
-                      screenSize: 7.5,
-                      resolutionX: 800,
-                      resolutionY: 480,
-                      colorMode: 'BWR',
-                      mac: device.mac,
+                const AppSearchBar(
+                  placeholder: 'Please enter a tag name or MAC',
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      labelText: 'Search by MAC Address',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _searchQuery = value;
+                      });
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _startScan(bleProvider);
+                  },
+                  child: const Text('Scan for Devices'),
+                ),
+                const SizedBox(height: 20),
+                _isScanning
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : Consumer<BleDeviceProvider>(
+                        builder: (context, provider, child) {
+                          final filteredDevices = provider.devices.where((device) {
+                            return device.name
+                                    .toLowerCase()
+                                    .contains(_searchQuery.toLowerCase()) ||
+                                device.mac
+                                    .toLowerCase()
+                                    .contains(_searchQuery.toLowerCase());
+                          }).toList();
 
-                      battery: '${device.battery}%',
-                    );
-                  }).toList(),
-                );
-              },
+                          if (filteredDevices.isEmpty) {
+                            return const Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text('No matching devices found.'),
+                              ),
+                            );
+                          }
+
+                          return Column(
+                            children: filteredDevices.map((device) {
+                              return TagCard(
+                                title: device.name.isEmpty ? 'Unknown Device' : device.name,
+                                available: provider.isConnected &&
+                                    device.mac == provider.connectedDevice?.mac,
+                                screenSize: 7.5,
+                                resolutionX: 800,
+                                resolutionY: 480,
+                                colorMode: 'BWR',
+                                mac: device.mac,
+                                battery: '${device.battery}%',
+                              );
+                            }).toList(),
+                          );
+                        },
+                      ),
+              ],
             ),
           ),
-          const NavBar(selectedMenu: 'tags')
+          const Align(
+            alignment: Alignment.bottomCenter,
+            child: NavBar(selectedMenu: 'tags'),
+          ),
         ],
       ),
     );
   }
+
   @override
   void dispose() {
     _searchController.dispose();
