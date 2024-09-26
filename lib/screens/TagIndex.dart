ec29bc7bc4acdf5f732344e2907cce186ba515df
Najlae Lambaraa <najlae@CT-C02XN556JHD2.local>
2024-09-26 16:12:20 +0200

    add search and list tag

--- lib/screens/TagIndex.dart
+++ lib/screens/TagIndex.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/colors.dart';
+import 'package:flutter_application_1/providers/ble_device_provider.dart';
 import 'package:flutter_application_1/widgets/AppSearchBar.dart';
 import 'package:flutter_application_1/widgets/NavBar.dart';
 import 'package:flutter_application_1/widgets/TagCard.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/AppTitle.dart';
 
-class TagIndex extends StatelessWidget {
+class TagIndex extends StatefulWidget {
   const TagIndex({super.key});
+
+  @override
+  _TagIndexState createState() => _TagIndexState();
+}
+
+class _TagIndexState extends State<TagIndex> {
+  final TextEditingController _searchController = TextEditingController();
+  String _searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    final bleProvider = Provider.of<BleDeviceProvider>(context, listen: false);
+    bleProvider.startBleScan();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,40 +39,58 @@ class TagIndex extends StatelessWidget {
           const AppSearchBar(
             placeholder: 'Please enter a tag name or MAC',
           ),
+           Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search by MAC Address',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value;
+                });
+              },
+            ),
+          ),
           Container(
             margin: const EdgeInsets.only(
               left: 28,
               right: 28,
               top: 20,
             ),
-            child: const Column(
-              children: [
-                TagCard(
-                  title: 'Tag de fou',
-                  available: true,
-                  screenSize: 7.5,
-                  resolutionX: 800,
-                  resolutionY: 480,
-                  colorMode: 'BWR',
-                  mac: '35:38:0A:00:EC:5B',
-                  battery: '100%',
-                ),
-                TagCard(
-                  title: 'Tag de ouf',
-                  available: false,
-                  screenSize: 7.5,
-                  resolutionX: 800,
-                  resolutionY: 480,
-                  colorMode: 'BWR',
-                  mac: '35:38:0A:00:EC:5B',
-                  battery: '100%',
-                ),
-              ],
+            child: Consumer<BleDeviceProvider>(
+              builder: (context, provider, child) {
+                return Column(
+                  children: provider.devices.map((device) {
+                    return TagCard(
+                      title: device.name,
+                      available: provider.isConnected && device.mac == provider.connectedDevice?.mac,
+                      screenSize: 7.5,
+                      resolutionX: 800,
+                      resolutionY: 480,
+                      colorMode: 'BWR',
+                      mac: device.mac,
+
+                      battery: '${device.battery}%',
+                    );
+                  }).toList(),
+                );
+              },
             ),
           ),
           const NavBar(selectedMenu: 'tags')
         ],
       ),
     );
+  }
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
